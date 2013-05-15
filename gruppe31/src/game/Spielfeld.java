142528ec18d60f8f42d0272b8f76ec381562433a
Denise Ruhrberg <Denise.Ruhrberg@uni-duesseldorf.de>
2013-05-15 19:57:39 +0200

    labels[] Schleifen überarbeitet

--- gruppe31/src/game/Spielfeld.java
+++ gruppe31/src/game/Spielfeld.java
@@ -16,9 +16,7 @@ public class Spielfeld {
  	 * erstellt das Array
  	 * 0: freies Feld, 1: Baum, 2: Pudel, 3: Gegner
  	 */
- 	public static  JLabel[] level1() {
- 		
- 		int boxWidth = 20;
+ 	public static void level1() {
  		
  		/*erstellt verschiedene Icons für jedes Bild*/
 		Icon iconRasen = new ImageIcon("src/Game/Images/Rasen.jpg");
@@ -26,52 +24,48 @@ public class Spielfeld {
 		Icon iconPudel = new ImageIcon("src/Game/Images/Pudel.png");
 		Icon iconGegner = new ImageIcon("src/Game/Images/Gegner.png");
  		
+		JLabel[] labels=new JLabel[48];
+		
 		int [][] feld1 = {{1,1,1,1,1,1,1,1},{1,0,0,0,0,0,0,1},{1,0,0,0,0,0,0,1},{1,2,0,0,0,0,0,1},{1,0,0,0,0,0,0,1},{1,1,1,1,1,1,1,1}};
 		
 				for (int i = 0; i < feld1.length; i++) {	
 					    for (int j = 0; j < feld1[i].length; j++) {	
-					    	
+					    	  	
 					    	/*Versuch die Bilder an die passenden Stellen zu setzen*/
 					    	if (feld1[i][j]==0){
-					    		JLabel[] labels=new JLabel[48];
-								for (int i1=0;i1<10;i1++) {
+								for (int i1=0;i1<labels.length;i1++) {
 									labels[i1]=new JLabel(iconRasen);
-									frame.f.getContentPane().add(labels[i1], BorderLayout.CENTER);
+									frame.f.getContentPane().add(labels[i1],BorderLayout.CENTER);
 						            frame.f.pack();
 						            frame.f.setVisible(true);
 								}
 							}
 					    	else if (feld1[i][j]==1){
-					    		JLabel[] labels=new JLabel[48];
-								for (int i1=0;i1<10;i1++) {
+								for (int i1=0;i1<labels.length;i1++) {
 									labels[i1]=new JLabel(iconGrenze);
 									frame.f.getContentPane().add(labels[i1], BorderLayout.CENTER);
 						            frame.f.pack();
 						            frame.f.setVisible(true);
 								}
 							}
-					    	else if (feld1[i][j]==1){
-					    		JLabel[] labels=new JLabel[48];
-								for (int i1=0;i1<10;i1++) {
+					    	else if (feld1[i][j]==2){
+								for (int i1=0;i1<labels.length;i1++) {
 									labels[i1]=new JLabel(iconPudel);
 									frame.f.getContentPane().add(labels[i1], BorderLayout.CENTER);
 						            frame.f.pack();
 						            frame.f.setVisible(true);
 								}
 							}
-					    	else if (feld1[i][j]==1){
-					    		JLabel[] labels=new JLabel[48];
-								for (int i1=0;i1<10;i1++) {
+					    	else if (feld1[i][j]==3){
+								for (int i1=0;i1<labels.length;i1++) {
 									labels[i1]=new JLabel(iconGegner);
 									frame.f.getContentPane().add(labels[i1], BorderLayout.CENTER);
 						            frame.f.pack();
 						            frame.f.setVisible(true);
 								}
 							}
-		  
 					   }
 				}
-			return null;
  	}
 }
 
