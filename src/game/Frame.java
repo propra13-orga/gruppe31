e1cf97a33fdc7b7524e3e752d99968995209aaa0
Denise Ruhrberg <Denise.Ruhrberg@uni-duesseldorf.de>
2013-05-18 17:39:56 +0200

    auf eine Klasse reduziert, einfacher KeyListener
    mit Rückmeldung in der Konsole

--- src/game/Frame.java
+++ src/game/Frame.java
@@ -2,6 +2,7 @@ package game;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -13,20 +14,70 @@ import javax.swing.*;
  * sind anklickbar und bewirken das Schließen des Fensters,
  * oder das Öffnen des Spielfensters.
  */
-	public class Frame extends JFrame /*implements KeyListener*/ {
+	public class Frame extends JFrame {
 		
 	static String direction = System.getProperty("user.dir");
+	
+	static Icon iconRasen = new ImageIcon(direction+"/src/game/Images/Rasen.jpg");
+	static Icon iconGrenze = new ImageIcon(direction+"/src/game/Images/Grenze.jpg");
+	static Icon iconPudel = new ImageIcon(direction+"/src/game/Images/Pudel.jpg");
+	static Icon iconGegner = new ImageIcon(direction+"/src/game/Images/Gegner.jpg");
+	static Icon iconZiel = new ImageIcon(direction+"/src/game/Images/Ziel.jpg");
 	
 	public static JButton start;
 	public static JButton ende;
 	public static JButton schliessen;
 	
-	public static JFrame f = new Frame();
 	public static JFrame F = new Frame();
+	public static JFrame f = new Frame();
 	
+	static int Spielfigurx;
+	static int Spielfigury;
 	
+	static int[][] aktuellesSpielfeld;
+	
+	static final int[][] feld1 = {{1,1,1,1,1,1,1,1},
+								  {1,2,0,1,0,0,0,1},
+								  {1,0,0,1,0,0,0,4},
+								  {1,0,0,0,0,0,0,1},
+								  {1,0,0,1,0,0,0,1},
+								  {1,1,1,1,1,1,1,1}};
+	
+	static final int [][] feld2 = {{1,1,1,1,1,1,1,1},
+								   {1,0,0,0,0,0,0,1},
+								   {2,0,1,0,1,1,1,1},
+								   {1,1,1,0,0,0,0,1},
+								   {1,0,0,0,1,0,0,4},
+								   {1,1,1,1,1,1,1,1}};
+	
+	static final int [][] feld3 = {{1,1,1,1,1,1,1,1},
+									{1,0,3,0,0,0,0,1},
+									{1,0,0,0,1,0,0,5},
+									{1,1,1,0,1,0,0,1},
+									{2,0,0,0,1,0,3,1},
+									{1,1,1,1,1,1,1,1}};
+	
+	public void processKeyEvent(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) { 
+			System.out.println("es wurde links gedrückt");
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			System.out.println("es wurde rechts gedrückt");
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_UP) {
+			System.out.println("es wurde hoch gedrückt");
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			System.out.println("es wurde runter gedrückt");
+		}
+	}
+	
+	public Frame() {
+		aktuellesSpielfeld=feld1;
+	}
+		
 	/* ruft das Menüfenster auf*/
-	public static void main(String[] args) { 
+	public static void main(String[] args) { 		
 		Menufenster();
 	}
 	
@@ -63,167 +114,132 @@ import javax.swing.*;
 		ende.setBounds(400,400,200,40);
 		F.add(ende);
 		
-		/*registriert Mausklick auf Button start und öffnet das Spielfenster*/
-		 ActionListener alstart = new ActionListener() {
-		      public void actionPerformed( ActionEvent e ) {		
-		    	  Spielfenster();
-		      }
-		    };
 		
-		 ActionListener alende = new ActionListener() {
-		      public void actionPerformed( ActionEvent e ) {
-		    	  System.exit(0);
-		      }
-		    };
+		/*registriert Mausklick auf Button start und öffnet das Spielfenster*/
+		ActionListener alstart = new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {		
+				Spielfenster();
+		    }
+		};
+		
+		ActionListener alende = new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {
+				System.exit(0);
+		    }
+		};
 		    
-		 /*setzt die beiden Buttons auf das Menüfenster*/
-		 start.addActionListener(alstart);
-		 ende.addActionListener(alende);
-		 
-		/* F.addKeyListener(new KeyListener() {
-	            public void keyTyped(java.awt.event.KeyEvent e) { 
-	            	/* do nothing 
-	            }
-	            
-	            public void keyReleased(java.awt.event.KeyEvent e) {
-	            	/* do nothing  
-	            }
-
-	            public void keyPressed(java.awt.event.KeyEvent e) {
-	               if (e.getKeyCode() == KeyEvent.VK_LEFT) { 
-						if (feld1[Spielfigurx-1][Spielfigury] = 3) {
-							Verloren();
-						}
-						else if (feld1[Spielfigurx-1][Spielfigury] = 4) {
-							Spielfeld.level2();
-						}
-						else if (feld1[Spielfigurx-1][Spielfigury] = 5) {
-							Gewonnen();
-						}
-						else if (feld1[Spielfigurx-1][Spielfigury] = 0) {
-							Spielfigurx=Spielfigurx-1;
-						}
-				   }
-				   
-	               if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-	               			if (feld1[Spielfigurx+1][Spielfigury] = 3) {
-							Verloren();
-						}
-						else if (feld1[Spielfigurx+1][Spielfigury] = 4) {
-							Spielfeld.level2();
-						}
-						else if (feld1[Spielfigur+1][Spielfigury] = 5) {
-							Gewonnen();
-						}
-						else if (feld1[Spielfigurx+1][Spielfigury] = 0) {
-							Spielfigurx=Spielfigurx+1;
-						}
-				   }
-				   
-	               if (e.getKeyCode() == KeyEvent.VK_UP) {
-	               			if (feld1[Spielfigurx][Spielfigury-1] = 3) {
-							Verloren();
-						}
-						else if (feld1[Spielfigurx][Spielfigury-1] = 4) {
-							Spielfeld.level2();
-						}
-						else if (feld1[Spielfigur][Spielfigury-1] = 5) {
-							Gewonnen();
-						}
-						else if (feld1[Spielfigurx][Spielfigury-1] = 0) {
-							Spielfigury=Spielfigury-1;
-						}
-				   }
-				   
-	               if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-	               			if (feld1[Spielfigurx][Spielfigury+1] = 3) {
-							Verloren();
-						}
-						else if (feld1[Spielfigurx][Spielfigury+1] = 4) {
-							Spielfeld.level2();
-						}
-						else if (feld1[Spielfigur][Spielfigury+1] = 5) {
-							Gewonnen();
-						}
-						else if (feld1[Spielfigurx][Spielfigury+1] = 0) {
-							Spielfigury=Spielfigury+1;
-						}
-				   }
-	            } 
-				});*/
-	} 
+		/*setzt die beiden Buttons auf das Menüfenster*/
+		start.addActionListener(alstart);
+		ende.addActionListener(alende);
+	}
 	
-	
-	/* Eigenschaften des Spielfeldes, führt Methode level1 aus der Klasse Spielfeld aus*/
 	public static void Spielfenster() {
 		f.setResizable(false);
 		f.setTitle("Erna's Adventure");
-		f.setSize(805,630);
-		f.setLayout(null);
+		f.setSize(800,600);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
-	//	f.addKeyListener(this);
+		f.setLayout(null);
 		
+		aktuellesSpielfeld=feld1;
 		
-		Spielfeld.level1();
+		Levelaufruf(aktuellesSpielfeld);
 	}
+	
+	/* Eigenschaften des Spielfeldes, führt Methode level1 aus der Klasse Spielfeld aus*/
+	public static void Levelaufruf(int [][] feld) {
+		JPanel panel = new JPanel();
+		panel.setLayout(null);		
+		
+		/*erstellt so viele Labels, wie für Array benötigt*/
+		JLabel[] labels=new JLabel[48];
+		
+				for (int i = 0; i < feld.length; i++) {	
+					    for (int j = 0; j < feld[i].length; j++) {	
+					    	
+					    	/*fügt dem jeweiligen Wert das passende Icon (=Bild) hinzu*/
+					    	if (feld[i][j]==0){
+								labels[i]=new JLabel(iconRasen);
+								panel.add(labels[i]);
+								labels[i].setBounds(j*100,i*100,100,100);
+							}
+					    	else if (feld[i][j]==1){
+								labels[i]=new JLabel(iconGrenze);
+								labels[i].setBounds(j*100,i*100,100,100);
+								panel.add(labels[i]);  
+							}
+					    	else if (feld[i][j]==2){
+					    		Spielfigurx=i;
+					    		Spielfigury=j;
+								labels[i]=new JLabel(iconPudel);
+								labels[i].setBounds(j*100,i*100,100,100);
+								panel.add(labels[i]);
+							}
+					    	else if (feld[i][j]==4){
+								labels[i]=new JLabel(iconRasen);
+								labels[i].setBounds(j*100,i*100,100,100);
+								panel.add(labels[i]);
+							}
+						Frame.f.setContentPane(panel);
+					   }
+				}	
+ 	}
 	
 	public static void Gewonnen() {
 		
 		try{
-			f.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(direction+"/src/game/Images/Gewonnen.jpg")))));
+			F.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(direction+"/src/game/Images/Gewonnen.jpg")))));
 		}
 		catch(IOException a) {
 			System.out.println("das Bild kann nicht gefunden werden");
 		}
 		
-		f.setResizable(false);
-		f.setSize(800,600);
-		f.setLayout(null);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-		f.setLocationRelativeTo(null);
-		f.setVisible(true);	
+		F.setResizable(false);
+		F.setSize(800,600);
+		F.setLayout(null);
+		F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		F.setLocationRelativeTo(null);
+		F.setVisible(true);	
 		
 		schliessen = new JButton("Dieses Fenster schließen");
 		schliessen.setBounds(550,450,200,40);
-		f.add(schliessen);
+		F.add(schliessen);
 		
-		/*registriert Mausklick auf Button start und öffnet das Spielfenster*/
-		 ActionListener alschliessen = new ActionListener() {
-		      public void actionPerformed( ActionEvent e ) {		
-		    	  f.setVisible(false);
-		      }
-		    };
+		ActionListener alschliessen = new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {		
+				F.setVisible(false);
+		    }
+		};
+		
 		schliessen.addActionListener(alschliessen);
 	}
 	
 	public static void Verloren() {
 		
 		try{
-			f.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(direction+"/src/game/Images/GameOver.jpg")))));
+			F.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(direction+"/src/game/Images/GameOver.jpg")))));
 		}
 		catch(IOException a) {
 			System.out.println("das Bild kann nicht gefunden werden");
 		}
 		
-		f.setResizable(false);
-		f.setSize(800,600);
-		f.setLayout(null);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-		f.setLocationRelativeTo(null);
-		f.setVisible(true);	
+		F.setResizable(false);
+		F.setSize(800,600);
+		F.setLayout(null);
+		F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		F.setLocationRelativeTo(null);
+		F.setVisible(true);	
 		
 		schliessen = new JButton("Dieses Fenster schließen");
 		schliessen.setBounds(300,450,200,40);
-		f.add(schliessen);
+		F.add(schliessen);
 		
-		/*registriert Mausklick auf Button start und öffnet das Spielfenster*/
-		 ActionListener alschliessen = new ActionListener() {
-		      public void actionPerformed( ActionEvent e ) {		
-		    	  f.setVisible(false);
-		      }
-		    };
+		ActionListener alschliessen = new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {		
+				F.setVisible(false);
+		    }
+		};	
 		schliessen.addActionListener(alschliessen);
 	}
 } 
