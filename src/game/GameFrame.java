700c7cb2bf5857806a1443781507d4bcef69f079
Denise Ruhrberg <Denise.Ruhrberg@uni-duesseldorf.de>
2013-06-04 12:51:16 +0200

    Veloren&Gewonnen wieder eingelagert
    bisher fehlende Parameterübergabe ! nur Verloren möglich

--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -1,13 +1,15 @@
 /*
- * Double-Buffering einbauen
- * http://www.rpg-studio.de/scientia/Java/Tutorials/Double_Buffering
- * 
  * oder Panels nur verschieben, anstatt Werte zu ändern und neu zu erstellen
+ * die betroffenen Felder herausfinden und diese beiden dann vertauschen(=Methode)
  */
 package game;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -20,8 +22,6 @@ public class GameFrame extends JFrame implements KeyListener {
 	private static final String direction = System.getProperty("user.dir");
 
 	Carlos carlos;
-	Gewonnen gewonnen;
-	Verloren verloren;
 
 	private static final int RASEN = 0;
 	private static final int GRENZE = 1;
@@ -52,39 +52,46 @@ public class GameFrame extends JFrame implements KeyListener {
 			+ "/src/game/Images/Carlos.png");
 	private static final Icon iconWaffe = new ImageIcon(direction
 			+ "/src/game/Images/Waffe1.png");
+	private static final Icon iconGewonnen = new ImageIcon(direction
+			+ "/src/game/Images/Gewonnen.jpg");
+	private static final Icon iconVerloren = new ImageIcon(direction
+			+ "/src/game/Images/Verloren.jpg");
 
 	/* Platzhalter für Marcel */
 	private static final Icon iconAnzeige = new ImageIcon(direction
 			+ "/src/game/Images/Anzeige.jpg");
 	private JLabel Anzeige = new JLabel(iconAnzeige);
 
+	private static final JLabel Gewonnen = new JLabel(iconGewonnen);
+	private static final JLabel Verloren = new JLabel(iconVerloren);
+
 	/* erstellt zwei Panels für die Bereiche des Spielfensters */
 	private Panel Wald = new Panel();
 	private Panel Leiste = new Panel();
+	private Panel Ausgang = new Panel();
+
+	private JButton GVschliessen;
 
 	/* Variabeln für die Position der Spielfigur */
-	int Spielfigurx;
-	int Spielfigury;
+	private int Spielfigurx;
+	private int Spielfigury;
 
 	/* deklariert das aktuelle Spielfeld */
-	int[][] aktuellesSpielfeld;
+	private int[][] aktuellesSpielfeld;
 	/* deklariert eine Variabel für das aktuelle Level */
-	int level;
+	private int level;
 
 	/* ein Array von Levels */
 	private static final int[][][] LEVELS = {
 			{
-				{ GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE,
-					GRENZE },
-			{ GRENZE, RASEN, RASEN, RASEN, RASEN, RASEN, CARLOS, GRENZE },
-			{ GRENZE, PUDEL, RASEN, RASEN, RASEN, RASEN, RASEN,
-					GRENZE },
-			{ GRENZE, RASEN, RASEN, RASEN, RASEN, RASEN, RASEN,
-					GRENZE },
-			{ GRENZE, RASEN, RASEN, RASEN, RASEN, RASEN, RASEN, WEITER },
-			{ GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE,
-					GRENZE } 
-			},
+					{ GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE,
+							GRENZE },
+					{ GRENZE, RASEN, RASEN, RASEN, RASEN, RASEN, CARLOS, GRENZE },
+					{ GRENZE, PUDEL, RASEN, RASEN, RASEN, RASEN, RASEN, GRENZE },
+					{ GRENZE, RASEN, RASEN, RASEN, RASEN, RASEN, RASEN, GRENZE },
+					{ GRENZE, RASEN, RASEN, RASEN, RASEN, RASEN, RASEN, WEITER },
+					{ GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE,
+							GRENZE } },
 
 			{
 					{ GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE, GRENZE,
@@ -222,7 +229,7 @@ public class GameFrame extends JFrame implements KeyListener {
 	/**
 	 * die Variabel level zählt hoch, um in das nächste Level zugelangen das
 	 * Spielfeld wird erneut durchlaufen und setzt das aktuelle Spielfeld = dem
-	 * geänderten
+	 * geänderten, Argumentübergabe (true/false?)
 	 */
 	protected void NextLevel() {
 		level++;
@@ -255,8 +262,42 @@ public class GameFrame extends JFrame implements KeyListener {
 		LevelAktualisieren();
 	}
 
+	public void Spielausgang() {
+
+		getContentPane().removeAll();
+
+		try {
+			this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File(
+					direction + "/src/game/Images/GameOver.jpg")))));
+		} catch (IOException a) {
+			System.out.println("das Bild kann nicht gefunden werden");
+		}
+
+		this.setResizable(false);
+		this.setSize(800, 600);
+		this.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+
+		/*
+		 * der Button schliessen bewirkt ein Verschwinden des Fensters, sodass
+		 * das Menufenster zu sehen ist
+		 */
+		GVschliessen = new JButton("Dieses Fenster schließen");
+		GVschliessen.setBounds(550, 450, 200, 40);
+		this.add(GVschliessen);
+
+		ActionListener alschliessen = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		};
+		GVschliessen.addActionListener(alschliessen);
+	}
+
 	/**
-	 * der KeyListener reagiert, während die Taste gedrückt wird
+	 * der KeyListener reagiert, während die Taste gedrückt wird verkürzen !
 	 */
 	public void keyPressed(KeyEvent e) {
 		/* fragt die KeyEvents ab und führt das Programm entsprechend weiter */
@@ -267,7 +308,7 @@ public class GameFrame extends JFrame implements KeyListener {
 			 */
 			if (aktuellesSpielfeld[Spielfigurx][Spielfigury - 1] == GEGNER) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			}
 			/*
 			 * wenn der Wert des abgefragten Feldes WEITER ist, erscheint das
@@ -282,7 +323,7 @@ public class GameFrame extends JFrame implements KeyListener {
 			 */
 			else if (aktuellesSpielfeld[Spielfigurx][Spielfigury - 1] == BOSS) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			}
 			/*
 			 * wenn der Wert des abgefragten Feldes ZURUECK ist, erscheint das
@@ -314,12 +355,12 @@ public class GameFrame extends JFrame implements KeyListener {
 		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			if (aktuellesSpielfeld[Spielfigurx][Spielfigury + 1] == GEGNER) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			} else if (aktuellesSpielfeld[Spielfigurx][Spielfigury + 1] == WEITER) {
 				NextLevel();
 			} else if (aktuellesSpielfeld[Spielfigurx][Spielfigury + 1] == BOSS) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			} else if (aktuellesSpielfeld[Spielfigurx][Spielfigury + 1] == ZURUECK) {
 				LastLevel();
 			} else if (aktuellesSpielfeld[Spielfigurx][Spielfigury + 1] == CARLOS) {
@@ -334,12 +375,12 @@ public class GameFrame extends JFrame implements KeyListener {
 		} else if (e.getKeyCode() == KeyEvent.VK_UP) {
 			if (aktuellesSpielfeld[Spielfigurx - 1][Spielfigury] == GEGNER) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			} else if (aktuellesSpielfeld[Spielfigurx - 1][Spielfigury] == WEITER) {
 				NextLevel();
 			} else if (aktuellesSpielfeld[Spielfigurx - 1][Spielfigury] == BOSS) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			} else if (aktuellesSpielfeld[Spielfigurx - 1][Spielfigury] == ZURUECK) {
 				LastLevel();
 			} else if (aktuellesSpielfeld[Spielfigurx - 1][Spielfigury] == CARLOS) {
@@ -354,12 +395,12 @@ public class GameFrame extends JFrame implements KeyListener {
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			if (aktuellesSpielfeld[Spielfigurx + 1][Spielfigury] == GEGNER) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			} else if (aktuellesSpielfeld[Spielfigurx + 1][Spielfigury] == WEITER) {
 				NextLevel();
 			} else if (aktuellesSpielfeld[Spielfigurx + 1][Spielfigury] == BOSS) {
 				getContentPane().removeAll();
-				verloren = new Verloren();
+				Spielausgang();
 			} else if (aktuellesSpielfeld[Spielfigurx + 1][Spielfigury] == ZURUECK) {
 				LastLevel();
 			} else if (aktuellesSpielfeld[Spielfigurx + 1][Spielfigury] == CARLOS) {
